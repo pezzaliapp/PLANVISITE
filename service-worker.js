1899b999edf9ee41ddb98bb72b066ca57ff46400
pezzaliapp <pezzalialessandro@gmail.com>
2025-01-22 16:08:09 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            // Return the cached response if found, else fetch from network
             return response || fetch(event.request);
         })
     );
